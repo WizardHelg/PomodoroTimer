80bf07da8a8342c7ee5e4f4769a89c069387ff98
Oleg Moiseev <wizardhelg@gmail.com>
2021-09-30 18:21:52 +0300

    Запрет запуска нескольких копий приложения

--- PomodoroTimer/Program.cs
+++ PomodoroTimer/Program.cs
@@ -1,16 +1,31 @@
 using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace PomodoroTimer
 {
     internal static class Program
     {
+        private static int c = 0;
+
+        public static bool IsSingle()
+        {
+            Mutex _ = new(true, "PomodoroMutex", out bool result);
+            return result;
+        }
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            if (!IsSingle())
+            {
+                MessageBox.Show("Другой экземпляр приложения уже запущен", "Pomodoro Timer");
+                return;
+            }
+
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
